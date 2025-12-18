ca51140a7f6776f550442fdfae7f2b720fd0086e
Aman Verma <amanverma1067@gmail.com>
2025-12-18 14:56:43 +0530

    Add: setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Aman Verma",
+    author_email="amanverma1067@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
